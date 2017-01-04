b07ac98d89389eb2a7566ceecf51d1408c827f21
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-04 21:50:01 +0700

    Method Chaining done all release dgana

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -1,39 +1,72 @@
- // The data store:
- var usersData = [{
-     firstName: "SpongeBob",
-     lastName: "SquarePants",
-     email: "spongebob@crustycrab.com",
-     id: 102
- }, {
-     firstName: "Patrick",
-     lastName: "Star",
-     email: "patric.star@gmail.com",
-     id: 103
- }, {
-     firstName: "Squidward",
-     lastName: "Tentacles",
-     email: "awesomesquidward@yahoo.com",
-     id: 104
- }]
+// The data store:
+var usersData = [{
+   firstName: "SpongeBob",
+   lastName: "SquarePants",
+   email: "spongebob@crustycrab.com",
+   id: 102
+}, {
+   firstName: "Patrick",
+   lastName: "Star",
+   email: "patric.star@gmail.com",
+   id: 103
+}, {
+   firstName: "Squidward",
+   lastName: "Tentacles",
+   email: "awesomesquidward@yahoo.com",
+   id: 104
+}]
 
- function titleCaseName(str) {
+let email = [];
+let key = ['firstName','lastName','email','id'];
+let userData = [];
 
+
+// Our object with the chainable methods using class in ES6
+class UserController {
+ constructor(){
+   this.firstName = '';
+   this.lastName = '';
+   this.email = '';
+   this.id = 0;
  }
 
- // Our object with the chainable methods using class in ES6
- class UserController {
-  
-   titleCaseName(str) {
-    
-    
+ findUser(str) {
+   for(let i = 0; i < usersData.length; i++){
+      email.push(usersData[i]['email']);
+      if(email[i] === str){
+        for(let j = 0; j < 4; j++){
+          userData.push(`${usersData[i][key[j]]}`)
+        }
+      }
    }
+   this.firstName = userData[0];
+   this.lastName = userData[1];
+   this.email = userData[2];
+   this.id = userData[3];
+  return this;
  }
 
- // Driver code
- let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ formatName() {
+   this.fullName = `${this.firstName} ${this.lastName}`;
+   return this;
+ }
 
- // result
- // Member name: Squidward Tentacles
- // ID: 104
- // Email: awesomesquidward@yahoo.com
+ formatData() {
+   this.formatData = `Member name: ${this.fullName} \nID: ${this.id} \nEmail: ${this.email}`;
+   return this;
+ }
+
+ displayUser() {
+   return console.log(this.formatData);
+ }
+}
+
+
+// Driver code
+let userController = new UserController;
+userController.findUser("patric.star@gmail.com").formatName().formatData().displayUser();
+
+// result
+// Member name: Squidward Tentacles
+// ID: 104
+// Email: awesomesquidward@yahoo.com
