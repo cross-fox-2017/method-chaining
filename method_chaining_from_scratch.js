013599b6cb7b7ba0baeeaeca9a7a2f7348c0ee5b
Syanmil <syanmil@yahoo.com>
2017-01-04 19:37:03 +0700

    masih bingung maksud soalnya, kenapa ada titleCaseName function atas

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -17,21 +17,45 @@
  }]
 
  function titleCaseName(str) {
-
+    for (var i = 0; i < str.length; i++) {
+      str[i].fullName = `${str[i].firstName} ${str[i].lastName}`
+    }
+   return str
  }
 
  // Our object with the chainable methods using class in ES6
  class UserController {
-  
+   constructor(){
+      this.usersData = usersData
+   }
    titleCaseName(str) {
-    
-    
+
+   }
+   findUser(str){
+      for (var i = 0; i < str.length; i++) {
+         if (usersData[i].email == str){
+            return this.usersData[i]
+         }
+      }
+      return `${str} is not found`
+   }
+   formatName(){
+      this.fullName = `${this.firstName} ${this.lastName}`
+      return this
+   }
+   formatData(){
+      return this
+   }
+   displayUser(){
+      return console.log(str)
    }
  }
 
  // Driver code
  let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ // console.log(titleCaseName(usersData));
+ console.log(userController.findUser("awesomesquidward@yahoo.com").formatName())
+ //.formatData().displayUser());
 
  // result
  // Member name: Squidward Tentacles
