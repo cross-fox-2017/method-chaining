7427e0f4c8976cb3a92e6dadf18c5457315a0bfb
pisanggoreng <7ofpentacles@gmail.com>
2017-01-07 20:43:14 +0700

    clear relase 0 & 1

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -16,22 +16,48 @@
      id: 104
  }]
 
- function titleCaseName(str) {
 
- }
 
  // Our object with the chainable methods using class in ES6
  class UserController {
-  
-   titleCaseName(str) {
-    
-    
+   constructor(usersDataParam) {
+     this.usersData = usersDataParam;
+   }
+
+   findUser(email) { // output = lokasi index ke 2
+     for ( var i = 0; i < this.usersData.length; i++ ) {
+       if ( this.usersData[i].email == email ) {
+         this.userIndex = i;
+         return this;
+       }
+     }
+   }
+
+   formatName() {
+     var index = this.userIndex;
+     this.nama = this.usersData[index].firstName + " " + this.usersData[index].lastName;
+     return this
+   }
+
+   formatData() {
+     // Member name: Squidward Tentacles
+     // ID: 104
+     // Email: awesomesquidward@yahoo.com
+     var index = this.userIndex;
+     this.dataNama = `Member name: ${this.nama} \nID: ${this.usersData[index].id} \nEmail: ${this.usersData[index].email}`
+     return this
+   }
+
+   displayUser() {
+     return this.dataNama
    }
  }
 
  // Driver code
- let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ let userController = new UserController(usersData);
+
+ console.log( userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser() );
+
 
  // result
  // Member name: Squidward Tentacles
