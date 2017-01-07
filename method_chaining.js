22dc4f9d6d3ae602d64dfcfa120bf57ac4164b72
Raditya <arya.raditya26@gmail.com>
2017-01-07 18:11:58 +0700

    selesai : release 0 dan 1

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,6 @@
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+  let a = data.toUpperCase().split("").reverse().join("");
+  return a + "!!!";
 }
 
 console.log(shout_backwards("Hello World"));
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -1,39 +1,74 @@
- // The data store:
- var usersData = [{
-     firstName: "SpongeBob",
-     lastName: "SquarePants",
-     email: "spongebob@crustycrab.com",
-     id: 102
- }, {
-     firstName: "Patrick",
-     lastName: "Star",
-     email: "patric.star@gmail.com",
-     id: 103
- }, {
-     firstName: "Squidward",
-     lastName: "Tentacles",
-     email: "awesomesquidward@yahoo.com",
-     id: 104
- }]
+// The data store:
+var usersData = [{
+    firstName: "SpongeBob",
+    lastName: "SquarePants",
+    email: "spongebob@crustycrab.com",
+    id: 102
+}, {
+    firstName: "Patrick",
+    lastName: "Star",
+    email: "patric.star@gmail.com",
+    id: 103
+}, {
+    firstName: "Squidward",
+    lastName: "Tentacles",
+    email: "awesomesquidward@yahoo.com",
+    id: 104
+}]
 
- function titleCaseName(str) {
 
+let email = [];
+let dataOfUser = [];
+let userProperties = ['firstName', 'lastName', 'email', 'id'];
+
+// Our object with the chainable methods using class in ES6
+class UserController {
+ constructor(){
+   this.firstName = ''
+   this.lastName = ''
+   this.email = ''
+   this.id = 0
  }
 
- // Our object with the chainable methods using class in ES6
- class UserController {
-  
-   titleCaseName(str) {
-    
-    
+ findUser(userEmail){
+   for (var i = 0; i < usersData.length; i++) {
+     email.push(usersData[i]['email'])
+     if (email[i] == userEmail) {
+       for (var j = 0; j < 4; j++) {
+         dataOfUser.push( usersData[i][userProperties[j]])
+       }
+     }
    }
+
+   this.firstName = dataOfUser[0]
+   this.lastName = dataOfUser[1]
+   this.email = dataOfUser[2]
+   this.id = dataOfUser[3]
+
+   return this
  }
 
- // Driver code
- let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ formatName(){
+   this.name = `${this.firstName} ${this.lastName}`
+   return this;
+ }
 
- // result
- // Member name: Squidward Tentacles
- // ID: 104
- // Email: awesomesquidward@yahoo.com
+ formatData(){
+   this.formatData = `result \nMember name: ${this.name} \nID: ${this.id} \nEmail: ${this.email}`
+   return this;
+ }
+
+ displayUser(){
+   return console.log(this.formatData);
+ }
+
+}
+
+// Driver code
+let userController = new UserController;
+userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+
+// result
+// Member name: Squidward Tentacles
+// ID: 104
+// Email: awesomesquidward@yahoo.com
