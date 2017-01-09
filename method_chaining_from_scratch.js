7fed52593a5136e914e6a9119590b64c729f481e
mfpebrizal <ftm.pebrizal@gmail.com>
2017-01-09 22:16:58 +0700

    mothod-chaining mfpebrizal

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -16,22 +16,65 @@
      id: 104
  }]
 
- function titleCaseName(str) {
 
- }
+ // function titleCaseName(str) {
+ //
+ // }
 
  // Our object with the chainable methods using class in ES6
  class UserController {
-  
-   titleCaseName(str) {
-    
-    
+   constructor(){
+     this.indexSearched = 0
+     this.fullName = ''
+     this.formatTampilan = ''
    }
+  //  titleCaseName(str){
+   //
+  //  }
+
+   findUser(dataSearched){
+     for(var i=0; i<usersData.length; i++){
+       if(dataSearched == usersData[i].firstName){
+         this.indexSearched = i
+         return this
+       }
+
+       if(dataSearched == usersData[i].lastName){
+         this.indexSearched =i
+         return this
+       }
+
+       if(dataSearched == usersData[i].email){
+         this.indexSearched = i
+         return this
+       }
+
+       if(dataSearched == usersData[i].id){
+         this.indexSearched = i
+         return this
+       }
+     }
+   }
+
+   formatName(){
+     this.fullName = `${usersData[this.indexSearched].firstName} ${usersData[this.indexSearched].lastName}`
+     return this
+   }
+
+   formatData(){
+     this.formatTampilan = `Member name : ${this.fullName}\nID : ${usersData[this.indexSearched].id}\nEmail : ${usersData[this.indexSearched].email}`
+     return this
+   }
+
+   displayUser(){
+     return this.formatTampilan
+   }
+
  }
 
  // Driver code
  let userController = new UserController;
- userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+ console.log(userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser());
 
  // result
  // Member name: Squidward Tentacles
