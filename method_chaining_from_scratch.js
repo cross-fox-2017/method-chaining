7ccded1fe6fd9c00502d8c8a46b4648c90917456
Eri Irawan <email": null>
2017-01-10 19:18:56 +0700

    Done all

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -15,27 +15,40 @@
      email: "awesomesquidward@yahoo.com",
      id: 104
  }]
-
- function titleCaseName(str) {
-
- }
+ // function titleCaseName(str) {
+ //
+ // }
 
  // Our object with the chainable methods using class in ES6
  class UserController {
 
    constructor() {
-     this.input   = userData;
+     this.input   = usersData;
      this.Temp    = "";
-     this.reulst  = [];
+     this.result  = [];
+   }
+
+   findUser(str){
+     this.str = str;
+     return this;
    }
 
    formatName(){
      for (let i = 0; i < this.input.length; i++) {
-       if(this.string == this.input[i]){
-
+       if(this.str == this.input[i]["email"]){
+         this.Temp = i;
+         return this;
        }
-
      }
+   }
+
+   formatData(){
+     this.result.push(this.input[this.Temp]);
+     return this;
+   }
+
+   displayUser(){
+     console.log(`Member Name : ${this.result[0]['firstName']} ${this.result[0]['lastName']}\nID : ${this.result[0]['id']}\nEmail : ${this.result[0]['email']}`)
    }
  }
 
