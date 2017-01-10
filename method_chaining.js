664e944e234038649df15741ca531f57d0d737c8
kahmadsyt <achmadkamil17@gmail.com>
2017-01-10 21:31:14 +0700

    Tugas Method Chaining

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,6 @@
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
-  return backwards + "!!!";
+  var all_caps = data.toUpperCase().split('').reverse().join('');
+  return all_caps + "!!!";
 }
 
 console.log(shout_backwards("Hello World"));
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -16,16 +16,45 @@
      id: 104
  }]
 
- function titleCaseName(str) {
-
- }
-
- // Our object with the chainable methods using class in ES6
+  // Our object with the chainable methods using class in ES6
  class UserController {
-  
-   titleCaseName(str) {
-    
-    
+   constructor(){
+     this.firstName  = ''
+     this.lastName   = ''
+     this.fullName   = ''
+     this.id         = 0
+     this.email      = ''
+     this._formatData = []
+   }
+
+   findUser(email) {
+     for(var i = 0; i < usersData.length; i++){
+       if(usersData[i].email == email){
+         this.firstName = usersData[i].firstName
+         this.lastName  = usersData[i].lastName
+         this.id        = usersData[i].id
+         this.email     = usersData[i].email
+       }
+     }
+     return this;
+   }
+
+   formatData() {
+     this._formatData.push(this.fullName)
+     this._formatData.push(this.id)
+     this._formatData.push(this.email)
+
+     return this;
+   }
+
+   displayUser() {
+     console.log(`Member Name : ${this._formatData[0]} \n ID : ${this._formatData[1]} \n Email : ${this._formatData[2]} `)
+     return this;
+   }
+
+   formatName() {
+     this.fullName = this.firstName + ' ' + this.lastName
+     return this;
    }
  }
 
